2bfab35035f28cf0f7a0508bb89be31a6797f565
cabbage <1255651384@qq.com>
2022-07-24 17:21:06 +0800

    fix: 修改

--- src/index.tsx
+++ src/index.tsx
@@ -2,16 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import { loadServer, DevTools } from "jira-dev-tool";
 import reportWebVitals from "./reportWebVitals";
-import { loadDevTools } from "jira-dev-tool";
 import { AppProviders } from "context";
 import "antd/dist/antd.less";
 
-loadDevTools(() => {
+loadServer(() => {
   ReactDOM.render(
-    <AppProviders>
-      <App />,
-    </AppProviders>,
+    <React.StrictMode>
+      <AppProviders>
+        <DevTools />
+        <App />,
+      </AppProviders>
+    </React.StrictMode>,
     document.getElementById("root") as HTMLElement
   );
 });
