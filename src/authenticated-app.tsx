d44c0b3ec49ef0a94c2b735a939bdb54d0562d2c
cabbage <1255651384@qq.com>
2022-07-24 13:42:02 +0800

    feat: 使用grid布局进行页面样式优化

--- src/authenticated-app.tsx
+++ src/authenticated-app.tsx
@@ -1,12 +1,52 @@
+import styled from "@emotion/styled";
+import { Button } from "antd";
+import { Row } from "components/lib";
 import { useAuth } from "context/auth-context";
 import { ProjectListScreen } from "screens/project-list";
 
 export const AuthenticatedApp = () => {
   const { logout } = useAuth();
   return (
-    <div>
-      <button onClick={logout}>登出</button>
-      <ProjectListScreen />
-    </div>
+    <Container>
+      <Header between={true}>
+        <HeaderLeft gap={true}>
+          <h3>logo</h3>
+          <h3>项目</h3>
+          <h3>用户</h3>
+        </HeaderLeft>
+        <HeaderRight>
+          <Button type="primary" onClick={logout}>
+            登出
+          </Button>
+        </HeaderRight>
+      </Header>
+      <Main>
+        <ProjectListScreen />
+      </Main>
+    </Container>
   );
 };
+
+const HeaderItem = styled.h3`
+  margin-right: 3rem;
+`;
+
+const Container = styled.div`
+  display: grid;
+  grid-template-rows: 6rem 1fr 6rem;
+  height: 100vh;
+`;
+
+const Header = styled(Row)`
+  height: 6rem;
+`;
+
+const HeaderLeft = styled(Row)`
+  display: flex;
+`;
+
+const HeaderRight = styled.div``;
+
+const Main = styled.main`
+  height: calc(100vh - 6rem);
+`;
